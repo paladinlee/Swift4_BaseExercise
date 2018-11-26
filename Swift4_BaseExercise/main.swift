2d03ce906bc2c04af27fcd9447cc774affab03bf
李志堅 <paladinlee@gmail.com>
2018-11-26 22:02:54 +0800

    Initial Commit

---
+++ Swift4_BaseExercise/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift4_BaseExercise
+//
+//  Created by paladin on 2018/11/26.
+//  Copyright © 2018年 paladin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
